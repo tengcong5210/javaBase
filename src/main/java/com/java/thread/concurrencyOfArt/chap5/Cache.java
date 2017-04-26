97a28746d0dd8ae4e8396255fb6278fcd20e2368
tengcong5210 <18616231631@126.com>
2017-04-26 22:00:22 +0800

    【java并发编程的艺术chap5】读写锁示例（缓存锁）

---
+++ src/main/java/com/java/thread/concurrencyOfArt/chap5/Cache.java
@@ -0,0 +1,45 @@
+package com.java.thread.concurrencyOfArt.chap5;
+
+import org.apache.commons.collections.map.HashedMap;
+
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * 缓存读写锁
+ * Created by ibm on 2017/4/26.
+ */
+public class Cache {
+    static Map<String,Object> map=new HashedMap();
+    static ReentrantReadWriteLock rwl=new ReentrantReadWriteLock();
+    static Lock r=rwl.readLock();
+    static Lock w=rwl.writeLock();
+    //获取一个key对应的value
+    public static final Object get(String key){
+        r.lock();
+        try {
+            return map.get(key);
+        }finally {
+            r.unlock();
+        }
+    }
+    //设置key对应的value,并返回旧的value
+    public static final Object put(String key,Object value){
+        w.lock();
+        try {
+            return map.put(key,value);
+        }finally {
+            w.unlock();
+        }
+    }
+    //清空所有的内容
+    public static final void clear(){
+        w.lock();
+        try {
+            map.clear();
+        }finally {
+            w.unlock();
+        }
+    }
+}
