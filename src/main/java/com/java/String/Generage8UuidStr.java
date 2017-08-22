13ae954676992f27fdc9c7fc86a89bf0ba3cee57
tengcong5210 <18616231631@126.com>
2017-08-22 11:29:25 +0800

    生成8位短uuid

---
+++ src/main/java/com/java/String/Generage8UuidStr.java
@@ -0,0 +1,34 @@
+package com.java.String;
+
+import java.util.UUID;
+
+/**
+ * 生成8位短uuid 参考:http://blog.csdn.net/andy_miao858/article/details/9530245
+ * @author tengcongcong
+ * @create 2017-08-18 16:20
+ * @Version 1.0
+ **/
+public class Generage8UuidStr {
+    public static String[] chars = new String[] { "a", "b", "c", "d", "e", "f",
+            "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+            "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5",
+            "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I",
+            "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+            "W", "X", "Y", "Z" };
+
+    public static String generateShortUuid() {
+        StringBuffer shortBuffer = new StringBuffer();
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        for (int i = 0; i < 8; i++) {
+            String str = uuid.substring(i * 4, i * 4 + 4);
+            int x = Integer.parseInt(str, 16);
+            shortBuffer.append(chars[x % 0x3E]);
+        }
+        return shortBuffer.toString();
+
+    }
+
+    public static void main(String[] args){
+        System.out.println("生成结果:"+generateShortUuid());
+    }
+}
