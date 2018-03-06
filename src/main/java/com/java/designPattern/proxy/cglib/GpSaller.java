dd08355ad3964542ed30775ce14e7819512613d6
tengcong5210 <18616231631@126.com>
2018-03-07 06:47:54 +0800

    添加注释

--- src/main/java/com/java/designPattern/proxy/cglib/GpSaller.java
+++ src/main/java/com/java/designPattern/proxy/cglib/GpSaller.java
@@ -42,6 +42,12 @@ public class GpSaller implements MethodInterceptor {
      */
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
         System.out.println("给土豆装饰一下，大小分开");
+        //这个obj的引用是由CGLib给我们new出来的
+        //cglib new出来以后的对象，是被代理对象的子类（继承了我们自己写的那个类）
+        //OOP, 在new子类之前，实际上默认先调用了我们super()方法的，
+        //new了子类的同时，必须先new出来父类，这就相当于是间接的持有了我们父类的引用
+        //子类重写了父类的所有的方法
+        //我们改变子类对象的某些属性，是可以间接的操作父类的属性的
         methodProxy.invokeSuper(o,objects);
         System.out.println("抽走我的提成");
         return null;
