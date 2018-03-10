e777034d60a34adfb2363f7540e6b20d1bd26c13
tengcong5210 <18616231631@126.com>
2018-03-11 07:00:55 +0800

    委派模式

---
+++ src/main/java/com/java/designPattern/delegate/BaseWorkerA.java
@@ -0,0 +1,7 @@
+package com.java.designPattern.delegate;
+
+public class BaseWorkerA implements Worker {
+    public void doing() {
+        System.out.println("基层员工A开始工作");
+    }
+}
---
+++ src/main/java/com/java/designPattern/delegate/delegateTest.java
@@ -0,0 +1,11 @@
+package com.java.designPattern.delegate;
+
+public class delegateTest {
+    public static void main(String [] args){
+        //看上去好像是我们的项目经理在干活
+        //但实际干活的人是普通员工
+        //这就是典型，干活是我的，功劳是你的
+        Manager manager=new Manager(new BaseWorkerB());
+        manager.doing();
+    }
+}
