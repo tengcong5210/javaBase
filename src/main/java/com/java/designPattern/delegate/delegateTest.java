8dc7f9a1bc51838994912640e863d6e0d573d20e
tengcong5210 <18616231631@126.com>
2018-03-11 07:02:05 +0800

    委派模式

--- src/main/java/com/java/designPattern/delegate/delegateTest.java
+++ src/main/java/com/java/designPattern/delegate/delegateTest.java
@@ -5,6 +5,7 @@ public class delegateTest {
         //看上去好像是我们的项目经理在干活
         //但实际干活的人是普通员工
         //这就是典型，干活是我的，功劳是你的
+        //spring 中在哪里有用到 委派模式？ 答：在注册IOC容器的时候
         Manager manager=new Manager(new BaseWorkerB());
         manager.doing();
     }
