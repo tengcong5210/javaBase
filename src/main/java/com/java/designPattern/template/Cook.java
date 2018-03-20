8ecdd010dc120f37fef331df3913dd8139eed9f3
tengcc <tengcc@shitou.com>
2018-03-20 19:01:45 +0800

    模板方法设计模式

---
+++ src/main/java/com/java/designPattern/template/Cook.java
@@ -0,0 +1,11 @@
+package com.java.designPattern.template;
+
+/**
+ * 烹饪
+ * @author tengcongcong
+ * @create 2018-03-14 9:04
+ * @Version 1.0
+ **/
+public interface Cook {
+    void doCook();
+}
---
+++ src/main/java/com/java/designPattern/template/TestDiGuoJi.java
@@ -0,0 +1,13 @@
+package com.java.designPattern.template;
+
+/**
+ * @author tengcongcong
+ * @create 2018-03-20 18:59
+ * @Version 1.0
+ **/
+public class TestDiGuoJi {
+    public static void main(String[] args){
+        Cook diGuoJi=new DiGuoJi();
+        diGuoJi.doCook();
+    }
+}
