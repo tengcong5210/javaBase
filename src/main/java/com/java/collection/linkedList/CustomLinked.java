3f94a6f7d063c6e30f9eeea544366da9b70d9502
tengcong5210 <18616231631@126.com>
2018-04-15 17:00:22 +0800

    手写linkedList添加注释

--- src/main/java/com/java/collection/linkedList/CustomLinked.java
+++ src/main/java/com/java/collection/linkedList/CustomLinked.java
@@ -22,14 +22,19 @@ public class CustomLinked<E> {
     }
 
     private void linkLast(E e){
+        //将尾节点 赋值给一个 临时节点
         final Node<E> l=last;
+        //创建一个新的节点，尾节点的临时变量作为新节点的前一个节点，插入元素作为新节点的数据元素，null作为新节点的为节点
         final Node<E> newNode=new Node<E>(l,e,null);
+        //将新创建的节点赋值给last节点
         last=newNode;
+        //如果临时节点为null，将新创的节点赋值给首节点，否则 将临时节点的下个节点指向新创建的节点
         if(l==null){
             first=newNode;
         }else{
             l.next=newNode;
         }
+        //集合大小++
         size++;
 
     }
