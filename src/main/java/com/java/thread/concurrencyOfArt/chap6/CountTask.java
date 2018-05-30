7ea0ac3605e0e1ba1be822283168fe94a1dd90a7
tengcong5210 <18616231631@126.com>
2018-05-31 07:01:57 +0800

    Fork/Join框架的使用demo

--- src/main/java/com/java/thread/concurrencyOfArt/chap6/CountTask.java
+++ src/main/java/com/java/thread/concurrencyOfArt/chap6/CountTask.java
@@ -44,7 +44,9 @@ public class CountTask extends RecursiveTask<Integer> {
 
     public static void main(String[] args) {
         ForkJoinPool forkJoinPool=new ForkJoinPool();
+        //生成一个计算任务 ，负责计算1+2+3+4
         CountTask task=new CountTask(1,4);
+        //执行一个任务
         Future<Integer> result=forkJoinPool.submit(task);
         try {
             System.out.print(result.get());
